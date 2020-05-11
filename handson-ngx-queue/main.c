b3e61bd4958025b106af5ff9fda591d135cfc5de
goro9 <sokenbiccha@icloud.com>
2020-05-11 11:01:20 +0900

    add ngx-queue practice

--- handson-ngx-queue/main.c
+++ handson-ngx-queue/main.c
@@ -4,6 +4,12 @@
 #include <string.h>
 #include "ngx-queue.h"
 
+#define ngx_queue_move(h, x)                                                                    \
+    (h)->next = (x)->next;                                                                      \
+    (h)->prev = (x)->prev;                                                                      \
+    (h)->next->prev = h;                                                                        \
+    (h)->prev->next = h
+
 typedef struct {
     uint8_t sample8;
     ngx_queue_t queue;
@@ -46,19 +52,22 @@ int main(int argc, char* argv[]) {
     ngx_queue_t que;
     create_queue(&que);
 
-    ngx_queue_t *last = NULL;
-    ngx_queue_t *q;
-    ngx_queue_foreach(q, &que)
-    {
-        if(q->next == &que) {
-            last = q;
-        }
-    }
-    s_que = que;
-    s_que.next->prev = &s_que;
-    last->next = &s_que;
-
+    // ngx_queue_t *last = NULL;
     // ngx_queue_t *q;
+    // ngx_queue_foreach(q, &que)
+    // {
+    //     if(q->next == &que) {
+    //         last = q;
+    //     }
+    // }
+    // s_que = que;
+    // s_que.next->prev = &s_que;
+    // last->next = &s_que;
+
+    // ngx_queue_move(&s_que, &que);
+    ngx_queue_insert_tail(&s_que, &que);
+
+    ngx_queue_t *q;
     ngx_queue_foreach(q, &s_que)
     {
         sample_queue_a_t *a = ngx_queue_data(q, sample_queue_a_t, queue);
